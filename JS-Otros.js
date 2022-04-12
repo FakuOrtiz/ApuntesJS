ab8675f27ede4894a8322a42dae516593e09cc80
Facundo Ortiz <facundoagustinortiz@gmail.com>
2022-04-12 11:28:22 -0300

    Agregado setTimeout() y setInterval()

--- JS-Otros.js
+++ JS-Otros.js
@@ -12,10 +12,14 @@ for (let i = 0; i < 10; i++) {
     console.log(i); //0, 1, 2, 3, 4, 6, 7, 8, 9
 }
 
+
+
 //Encontrar caracter en un String----------------------------------------------
 let nombre = "Facundo";
 let primerCaracter = nombre[0];
 let ultimoCaracter = nombre[nombre.length-1];
+
+
 
 //Operador ternario (if-else de una sola línea)---------------------------------
 //condición ? código true : código false;
@@ -23,12 +27,16 @@ num1 = 10;
 num2 = 5;
 num1 > num2 ? `${num1} es mayor que ${num2}` : `${num2} es mayor que ${num1}`;
 
+
+
 //Función flecha----------------------------------------------------------------
 //En este caso no son necesarias las {} porque es de una sola línea
 let array1 = [1, 2, 3]; 
 let array2 = [4, 5, 6];
 let concatenar = (arr1, arr2) => arr1.concat(arr2);
 console.log(concatenar(array1, array2));
+
+
 
 //Expresiones Regulares---------------------------------------------------------
 //Patrones utilizados para encontrar una determinada combinación de caracteres dentro de una cadena de texto.
@@ -68,3 +76,10 @@ Grupos
 ( )     - Grupo
 |       - Uno u otro**/
 
+
+
+//setTimeout() y setInterval()--------------------------------------------------
+setTimeout(() => console.log("Hola"), 10000); //setTimeout(funcion, milisegs)
+let interval = setInterval(() => console.log("Chau"), 3000); //setInterval(funcion, milisegs)
+//Esto para que no corra infinitamente, se guarda el setInterval en una variable
+setTimeout(() => clearInterval(interval), 10000);
